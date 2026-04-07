0df1408eef2f1a059c5f324408257c5b1c05e318
user.name malhotra <user.email spiderwebmr76@gmail.com>
2026-04-07 21:16:44 +0530

    aaj ka code - 07 April 2026

--- server/controller/website.controllers.js
+++ server/controller/website.controllers.js
@@ -1,3 +1,18 @@
-export const generateWebsite = async (req , res) =>{
-    
+export const generateWebsite = async (req, res) => {
+
+ try {
+  const { prompt } = req.body
+  if (!prompt) {
+   return res.status(400).json({ message: " prompt is required " })
+
+  }
+
+  const user = await User
+  if(!user){
+   return res.status(400).json({ message:" user not found"})
+  }
+ } catch (error) {
+
+ }
+
 }
